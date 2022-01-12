d6e3f3fa45c4697bfbd556208b419abe8bfe3b9e
Khanetsky-Zhenya <khanetsky.zhenya@mail.ru>
2022-01-12 21:03:10 +0300

    add: add header in component header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -2,10 +2,24 @@ import React from 'react'
 
 import classes from './styles.module.scss'
 
+import logo from '../../assets/img/logo.png'
+
 const Header = () => {
   return (
     <div className={classes.wrapper}>
-      Header
+      <div className={classes.body}>
+        <img className={classes.header_logo} src="" alt='логотип'/>
+          <ul className={classes.header_nav}>
+            <li className={classes.header_nav_item}>Поиск водоема</li>
+            <li className={classes.header_nav_item}>Все водоемы</li>
+            <li className={classes.header_nav_item}>Рыбаки</li>   
+          </ul>
+        <div className={classes.header_authorization}>
+          <img className={classes.header_authorization_userIcon} src='../../assets/img/userIcon.png' alt='иконка пользователя'/>
+          <p className={classes.header_authorization_userFirstName}>Дима</p>
+          <p className={classes.header_authorization_userSecondName}>Долидов</p>
+        </div>
+      </div>
     </div>
   )
 }
