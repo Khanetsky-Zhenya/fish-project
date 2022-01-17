a21826a8549e4dee304d54ae2fb98eeba3688812
dolidov <ddolidov@ifuture.by>
2022-01-17 22:25:16 +0300

    Replaced TextTemplate component

--- pages/MainPage/components/TextTemplate/index.tsx
+++ pages/MainPage/components/TextTemplate/index.tsx
@@ -2,14 +2,13 @@ import React from 'react'
 
 import classes from './styles.module.scss'
 
-
 const TextTemplate = (props) => {
     const { pretitle, title, subtitle } = props;
   return (
 <div>
   <div className={classes.pre_text}>
     <div className={classes.tire}></div>
-    <p className={classes.pre_title2}>{pretitle}</p>
+    <p className={classes.pre_title3}>{pretitle}</p>
   </div>
 <h2 className={classes.title}>{title}</h2>
 <p className={classes.subtitle}>{subtitle}</p>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,19 @@
 import classes from './MainPage/styles.module.scss'
 import PageWrapper from "../components/layouts/PageWrapper"
 import BtnGreen from 'components/BtnGreen'
-import TextTemplate from 'components/TextTemplate'
+import TextTemplate from './MainPage/components/TextTemplate'
 
-export default function Home() {
+const Home = () => {
   return (
     <PageWrapper>
       <div className={classes.wrap}>
       <div className={classes.body}>
         <div className={classes.body_left}>
-        <TextTemplate pretitle="станьте участником уже сегодня!" title="давай отправимся на рыбалку!" subtitle="Мы рады поддерживать и продвигать рыбалку как вид спорта и здоровый образ жизни во всем мире. Мы помогаем новичкам и опытным членам нашего клуба."/>
+        <TextTemplate
+          pretitle="станьте участником уже сегодня!"
+          title="давай отправимся на рыбалку!"
+          subtitle="Мы рады поддерживать и продвигать рыбалку как вид спорта и здоровый образ жизни во всем мире. Мы помогаем новичкам и опытным членам нашего клуба."
+        />
           <div className={classes.buttonGroup}>
             <div className={classes.buttonGroup_left}><BtnGreen name="найти водоем"/></div>
             <div className={classes.buttonGroup_right}><BtnGreen name="стать членом клуба"/></div>
@@ -21,3 +25,5 @@ export default function Home() {
     </PageWrapper>
   )
 }
+
+export default Home
