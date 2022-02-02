a051cfdaab8831d8050b53cd55f4780c68cefe05
Khanetsky-Zhenya <khanetsky.zhenya@mail.ru>
2022-02-02 16:41:16 +0300

    add: Add info in page Reservoirs and add new component ReservoirCard

--- pages/reservoirs/index.tsx
+++ pages/reservoirs/index.tsx
@@ -1,9 +1,24 @@
 import PageWrapper from 'components/layouts/PageWrapper'
+import ReservoirCard from './components/ReservoirCard'
+
+import classes from './styles.module.scss'
 
 export const ReservoirsPage = () => {
   return (
     <PageWrapper>
-      <div>Водоемы</div>
+      <div className={classes.container}>
+        <h2 className={classes.title}>водоемы беларуси</h2>
+        <div className={classes.allRegionButton}>
+          <p className={classes.titleRegionButton}>Области:</p>
+          <button className={classes.regionButton}>Брестская</button>
+          <button className={classes.regionButton}>Витебская</button>
+          <button className={classes.regionButton}>Гомельская</button>
+          <button className={classes.regionButton}>Гродненская</button>
+          <button className={classes.regionButton}>Минская</button>
+          <button className={classes.regionButton}>Могилевская</button>
+        </div>
+        <ReservoirCard/>
+      </div>
     </PageWrapper>
   )
 }
